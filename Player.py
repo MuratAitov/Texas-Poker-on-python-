81780088149cf995a8af65e1a6e235fb0af2447b
Murat <140990101+MuratAitov@users.noreply.github.com>
2024-03-19 12:12:13 -0700

    abstract player

--- Player.py
+++ Player.py
@@ -1,8 +1,10 @@
-class Player:
+from abc import ABC, abstractmethod
+
+class Player(ABC):
     def __init__(self, name):
         self.name = name
         self.hand = []
-        self.chips = 0
+        self.chips = 1000
 
     def add_card(self, card):
         self.hand.append(card)
@@ -12,4 +14,8 @@ class Player:
             self.chips -= amount
             return amount
         else:
-            return 0  
+            return 0
+
+    @abstractmethod
+    def decide_action(self):
+        pass
