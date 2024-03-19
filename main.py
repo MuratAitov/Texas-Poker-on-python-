b93bc5a6c8fa21a54dabf9618b9dce7c3a28a32a
AitovM <aitov.munir24@mail.ru>
2024-03-19 21:53:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 import pygame
+import random
+
+window_size = (800, 600)
+window = pygame.display.set_mode(window_size)
+pygame.display.set_caption('Техасский покер')
+pygame.init()
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            quit()
+
+
+    pygame.display.update()
